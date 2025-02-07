8dc660cbcc8249f8b5673a9e13ac5e68c706f1c1
Kreuer-Neil <kreuer.neil@student.hepl.be>
2025-02-07 09:09:43 +0100

    Update

--- wp-content/themes/portfolio/header.php
+++ wp-content/themes/portfolio/header.php
@@ -7,7 +7,7 @@
     <meta name="author" content="Kreuer Neil"/>
     <?php wp_head() ?>
     <meta name="keyword" content="Portfolio, Neil Kreuer, Web Designer, CV, Web Developer">
-    <meta name="description" content="Nouvelle page d'accueil du Service d'Entraide Familiale, version de Neil Kreuer"/>
+    <meta name="description" content="Page d'accueil du site Portfolio "/>
 
     <?php wp_head(); ?>
 
@@ -16,17 +16,22 @@
 <body>
 
 <header>
-    <h1 ><span class="sro"><?= get_the_title(); ?></span><a href="<?= get_home_url() ?>" title="Vers l'accueil"><svg></svg></a></h1>
-    <?php //todo mettre le SVG dans le titre ?>
+    <h1 class="sro"><?= get_the_title(); ?></h1>
 
     <nav class="main-nav" id="main-nav">
         <h2 class="main-nav__title sro">Menu de navigation principale</h2>
-            <ul class="main-nav__container">
-                <?php foreach (dw_get_navigation_links('main') as $link): ?>
-                    <li class="main-nav__element<?php if ($link->url === get_page_link()) echo " main-nav__elements--active" ?>">
-                        <a href="<?= $link->url ?>" class="main-nav__links"><?= $link->label ?></a>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
+        <ul class="main-nav__container">
+            <li class="main-nav__element main-nav__element--home">
+                <a class="main-nav__link main-nav__link--home" href="<?= get_home_url() ?>" title="Vers l'accueil">
+                    <svg class="site-icon" title="Logo N K" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 181 129" role="img" width="50">
+                        <defs></defs><path class="site-icon--1" d="M167.58 1.45h-62.75v61.93l62.75-61.93z"/><path class="site-icon--2" d="m128.12 68.07 52.2 46.24V14.19l-52.2 53.88z"/><path class="site-icon--1" d="M104.83 89.45v38.19h62.75l-52.2-46.24-10.55 8.05z"/><path class="site-icon--2" d="M85.74 95.07V1.45H22.25l63.49 93.62z"/><path class="site-icon--1" d="M19.69 31.69v95.95h65.07L19.69 31.69z"/><path class="site-icon__stroke" d="M1 1h179v127H1z"/>
+                    </svg></a>
+            </li>
+            <?php foreach (dw_get_navigation_links('main') as $link): ?>
+                <li class="main-nav__element<?php if ($link->url === get_page_link()) echo " main-nav__element--active" ?>">
+                    <a href="<?= $link->url ?>" class="main-nav__link"><?= $link->label ?></a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
     </nav>
 </header>
--- wp-content/themes/portfolio/index.php
+++ wp-content/themes/portfolio/index.php
@@ -3,7 +3,7 @@
 $recent_projects = new WP_Query([
     'post_type' => 'project',
     'post_status' => 'publish',
-    'posts_per_page' => 3,
+    'posts_per_page' => 5,
     'orderby' => 'date',
     'order' => 'DESC',
 ]);
@@ -17,18 +17,19 @@ $recent_projects = new WP_Query([
             <section class="about" id="about">
                 <h2 class="about__title">Qui suis-je ?<span class="sro"> Petite biographie</span></h2>
 
-                <article class="about__short">
+                <article class="about__container--short">
                     <?php if(get_field('short_bio_title')): ?>
-                    <h3 class="about__short__title"><?= get_field('short_bio_title')?></h3>
+                    <h3 class="about__title--short"><?= get_field('short_bio_title')?></h3>
                     <?php endif; ?>
 
-                    <p class="about__short__content">
+                    <p class="about__content--short">
                         <?= get_field('short_bio_content'); ?>
                     </p>
                 </article>
                 <div class="about__quick-nav">
-                    <a href="#<?= get_field('projects_title')?>"
-                    <a href="#<?= get_field('form_title')?>" class="about__quick__contact no-js" title="Vers la page de contact">Me contacter</a>
+                    <?php //todo mettre les title et liens en wp ?>
+                    <a href="#projects">Vers les projets</a>
+                    <a href="#contact" class="about__quick__contact no-js" title="Vers la page de contact">Me contacter</a>
                     <?php
                     //todo mettre le lien façon WP (ou pas)
                     //todo faire apparaître avec une checkbox ? Le label serait le texte d'ouverture et la croix la checkbox (input type checkbox)
@@ -36,19 +37,30 @@ $recent_projects = new WP_Query([
                     //todo ou mettre directement dessous et toggle la visibility pour éviter problèmes d'accessibilité (voir comment boucler la nav dedans temporairement dans ce cas)
                     ?>
                 </div>
-                <article class="about__skills">
-                    <h2 class="about__skills__title"><?= get_field('skills_title') ?></h2>
+                <article class="about__container--skills">
+                    <h2 class="about__title--skills"><?= get_field('skills_title') ?></h2>
+                    <p class="about__content--skills"><?= get_field('skills_content')?></p>
+                </article>
+                <article class="about__container--specs">
+                    <h2 class="about__title--specs"><?= get_field('specialities_title') ?></h2>
+                    <p class="about__content--specs"><?= get_field('specialities_content')?></p>
                 </article>
             </section>
 
             <section class="projects" id="projects">
-                <h2 class="projects__title"
-                <div class="projects__container">
-                    <?php foreach ($recent_projects as $project) : ?>
-                        <article class="project__item">
+                <h2 class="projects__title">Mes projets</h2>
 
+                <div class="projects__container">
+                    <?php if($recent_projects->have_posts()): while($recent_projects->have_posts()): $recent_projects->the_post(); ?>
+                        <article class="project">
+                            <?php $image = get_field('project_thumbnail'); ?>
+                            <sh></sh>
+                            <h3 class="project__title"><?= get_the_title(); ?></h3>
+                            <p><?= get_field('short_description') ?></p>
+                            <p class="project__link"><a href="<?= get_page_link() ?>">Vers le projet</a></p>
+                            <img class="project__img" src="<?= $image['sizes']['thumbnail'] ?>" alt="<?= $image['alt'] ?>">
                         </article>
-                    <?php endforeach; ?>
+                    <?php endwhile; endif; ?>
                 </div>
             </section>
         </div>
